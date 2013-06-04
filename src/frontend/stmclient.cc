a91086667e13126226e80fc3c517d3194a390a62
Keith Winstein <keithw@mit.edu>
2013-06-04 17:38:54 +0200

    Parameter set to save bandwidth on ultra-constrained links

--- src/frontend/stmclient.cc
+++ src/frontend/stmclient.cc
@@ -254,7 +254,7 @@ void STMClient::main_init( void )
   Terminal::Complete local_terminal( window_size.ws_col, window_size.ws_row );
   network = NetworkPointer( new NetworkType( blank, local_terminal, key.c_str(), ip.c_str(), port.c_str() ) );
 
-  network->set_send_delay( 1 ); /* minimal delay on outgoing keystrokes */
+  network->set_send_delay( 400 ); /* minimal delay on outgoing keystrokes */
 
   /* tell server the size of the terminal */
   network->get_current_state().push_back( Parser::Resize( window_size.ws_col, window_size.ws_row ) );
